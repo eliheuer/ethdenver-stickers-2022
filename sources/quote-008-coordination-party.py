0e2cbea09231b829e05a9648a5567d3380390cd2
Eli Heuer <elih@protonmail.com>
2021-11-28 07:02:29 -0800

    add "coordination party" quote main source

---
+++ sources/quote-008-coordination-party.py
@@ -0,0 +1,61 @@
+# | drawbot.com | use drawbot-skia on GNU+Linux systems
+from drawBot import *
+
+
+# Constants, these are the main "settings" for the image
+WIDTH = 2048
+HEIGHT = 1024
+MARGIN = 128
+FRAMES = 1
+GRID_VIEW = True # Change this from "False" to "True" for a grid overlay
+
+
+# Draw a grid if the constant "GRID_VIEW" is set to "True"
+def grid():
+    stroke(1, 0, 0, 0.75)
+    strokeWidth(2)
+    STEP_X, STEP_Y = 0, 0
+    INCREMENT_X, INCREMENT_Y = MARGIN / 2, MARGIN / 2
+    rect(MARGIN, MARGIN, WIDTH - (MARGIN * 2), HEIGHT - (MARGIN * 2))
+    for x in range(29):
+        polygon((MARGIN + STEP_X, MARGIN), (MARGIN + STEP_X, HEIGHT - MARGIN))
+        STEP_X += INCREMENT_X
+    for y in range(12):
+        polygon((MARGIN, MARGIN + STEP_Y), (WIDTH - MARGIN, MARGIN + STEP_Y))
+        STEP_Y += INCREMENT_Y
+    polygon((WIDTH / 2, 0), (WIDTH / 2, HEIGHT))
+    polygon((0, HEIGHT / 2), (WIDTH, HEIGHT / 2))
+
+
+# Draw the page/frame and a grid if "GRID_VIEW" is set to "True"
+def draw_background():
+    newPage(WIDTH, HEIGHT)
+    fill(0)
+    rect(-2, -2, WIDTH + 2, HEIGHT + 2)
+    if GRID_VIEW:
+        grid()
+    else:
+        pass
+
+
+def draw_image():
+    draw_background()
+    fill(0.95)
+    stroke(None)
+    font("fonts/bobo/Bobo.ttf")
+    for axis, data in listFontVariations().items():
+        print((axis, data))
+    fontSize(144)
+    #text("let's throw a coordination party", (MARGIN-0.5, MARGIN + (0*MARGIN)))
+    text("Let's Throw a", (MARGIN-0.5, MARGIN + (2*MARGIN)))
+    text("Coordination Party", (MARGIN-0.5, MARGIN + (1*MARGIN)))
+
+    fontSize(1000)
+    font("fonts/clipart/Clipart-Regular.ttf")
+    text("A", (MARGIN, MARGIN + (5*MARGIN)))
+
+# Build and save the image
+if __name__ == "__main__":
+    draw_image()
+    saveImage("quote-008-coordination-party.png")
+    print("DrawBot: Done :-)")
